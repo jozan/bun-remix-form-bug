c1b4b46c40921f21423dd206803d3ec991f04468
Johan Ruokangas <johan@latehours.net>
2023-08-03 00:12:08 +0300

    tussi

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,31 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json, type ActionArgs, type V2_MetaFunction } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react"
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+    return [
+        { title: "New Remix App" },
+        { name: "description", content: "Welcome to Remix!" },
+    ];
 };
 
+export async function action({ request }: ActionArgs) {
+    console.log(request);
+    console.log(request.method);
+    console.log(await request.formData());
+
+    return json({ hello: "world" });
+}
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    const fetcher = useFetcher();
+    return (
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+            <fetcher.Form method="POST" action="/?index">
+                <button type="submit" name="form-type" value="fetcher form">fetcher form</button>
+            </fetcher.Form>
+            <form method="POST" action="/?index">
+                <button type="submit" name="form-type" value="form form">form form</button>
+            </form>
+        </div>
+    );
 }
